821a0e35d4deb481f22f75bf01c68ec4869e48ce
shawn <shawnchenofficial@gmail.com>
2022-07-16 01:10:52 +1200

    get post query

---
+++ teacher-api/Application/Posts/Queries/GetPostByCategory/GetPostsQuery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using teacher_api.Application.Base.Interface;
+using teacher_api.Application.Posts.Models;
+using teacher_api.Domain.Entities.Posts;
+
+namespace teacher_api.Application.Posts.Queries.GetPost
+{
+	public record GetPostQuery: IRequest<List<PostDto>>
+    {
+        [FromRoute]
+        public int CategoryId { get; set; }
+	}
+
+    public class GetPostQueryHandler : IRequestHandler<GetPostQuery, List<PostDto>>
+    {
+        private readonly IRepository<Post> postRepo;
+
+        public GetPostQueryHandler(IRepository<Post> postRepo)
+        {
+            this.postRepo = postRepo;
+        }
+
+        public Task<List<PostDto>> Handle(GetPostQuery request, CancellationToken cancellationToken)
+        {
+            var result = postRepo
+                .Get()
+                .Where(w => w.CategoryId == request.CategoryId)
+                .OrderByDescending(o => o.CreatedBy)
+                .Select(s => new PostDto
+                {
+                    CategoryId = s.CategoryId,
+                    StartDate = s.StartDate,
+                    CreatedBy = s.CreatedBy,
+                    Description = s.Description,
+                    Id = s.Id,
+                    Title = s.Title,
+                    UserId = s.UserId
+                }).ToList();
+
+            return Task.FromResult(result);
+        }
+    }
+}
+
